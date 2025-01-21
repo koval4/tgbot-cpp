9263c80df58b45b199d14305f99a96798a1454c6
Roman Kovalivskiy <roman.kovalivskiy@gmail.com>
2025-01-21 19:07:10 +0200

    Add smoke test
    Signed-off-by: Roman Kovalivskiy <roman.kovalivskiy@gmail.com>

--- libtgbot-cpp/tests/basics/driver.cpp
+++ libtgbot-cpp/tests/basics/driver.cpp
@@ -1,34 +1,5 @@
-#include <sstream>
-#include <stdexcept>
+#include <tgbot/tgbot.h>
 
-#include <tgbot/tgbot-cpp.h>
-
-#undef NDEBUG
-#include <cassert>
-
-int main ()
-{
-  using namespace std;
-  using namespace tgbot_cpp;
-
-  // Basics.
-  //
-  {
-    ostringstream o;
-    say_hello (o, "World");
-    assert (o.str () == "Hello, World!\n");
-  }
-
-  // Empty name.
-  //
-  try
-  {
-    ostringstream o;
-    say_hello (o, "");
-    assert (false);
-  }
-  catch (const invalid_argument& e)
-  {
-    assert (e.what () == string ("empty name"));
-  }
+int main() {
+    return 0;
 }
